3bece335fd98aa5bbc2575f07e41d5100f47f075
Baran <brn.okty@gmail.com>
2023-04-17 21:43:41 +0300

    Case Added

---
+++ Assets/Game/Scripts/CaseHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CaseHandler : MonoBehaviour
+{
+    public int requiredCarriableCount = 10;
+    [SerializeField] private TextMeshPro textMeshPro;
+    [SerializeField] private Transform leftStrip;
+    [SerializeField] private Transform rightStrip;
+    private int currentCarriableCount = 10;
+
+
+    public void AddCarriable()
+    {
+        currentCarriableCount++;
+
+        if (currentCarriableCount>=10)
+        {
+            textMeshPro.color=Color.green;
+        }
+
+        textMeshPro.text = currentCarriableCount + " / " + requiredCarriableCount;
+    }
+}
